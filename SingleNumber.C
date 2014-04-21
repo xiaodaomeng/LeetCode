0d22de055badb1a7f32e9526a9b0dc879ef88c66
xiaodaomeng <my85310@gmail.com>
2014-04-20 22:06:59 -0400

    Single Number

---
+++ SingleNumber.C
@@ -0,0 +1,17 @@
+/*
+Single Number
+Given an array of integers, every element appears twice except for one. Find that single one.
+
+Note:
+Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+*/
+class Solution {
+public:
+    //time O(n),space O(1)
+    int singleNumber(int A[], int n) {
+        for(int i=1;i<n;i++){
+            A[0]^=A[i];
+        }
+        return A[0];
+    }
+};

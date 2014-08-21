3d4587ad23ca65f6083650e76cc081c0c955c5ea
xiaodaomeng <my85310@gmail.com>
2014-08-21 16:39:14 -0400

    Remove Duplicates from Sorted Array II

---
+++ array/Remove_duplicates_from_sorted_array_II.C
@@ -0,0 +1,24 @@
+/*
+Remove Duplicates from Sorted Array II 
+Follow up for "Remove Duplicates":
+What if duplicates are allowed at most twice?
+
+For example,
+Given sorted array A = [1,1,1,2,2,3],
+
+Your function should return length = 5, and A is now [1,1,2,2,3].
+*/
+class Solution {
+public:
+    //method 1: for loop, time O(n),space O(1)
+    int removeDuplicates(int A[], int n) {
+        if(n<=2) return n;
+        int j=2;
+        for(int i=2;i<n;i++){
+            if(A[i]!=A[j-2]){
+                A[j++] = A[i];
+            }
+        }
+        return j;
+    }
+};
